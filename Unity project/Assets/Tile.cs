1bb35397e89ef7fbcededb36a0b80b467bfa2bc5
Matt Dodd <doddzy39@gmail.com>
2014-01-25 00:48:13 +1100

    Modified tiles to use new tile with pivot point

--- Unity project/Assets/Tile.cs
+++ Unity project/Assets/Tile.cs
@@ -84,28 +84,28 @@ public class Tile : MonoBehaviour
 		{
 			Color prevColour = Gizmos.color;
 			Gizmos.color = new Color(0, 252, 255, 255);
-			Gizmos.DrawWireCube(NorthTile.transform.position + new Vector3(0, 1, 0), new Vector3(4, 1, 4));
+			Gizmos.DrawWireCube(NorthTile.transform.position + new Vector3(0, 0.25f, 0), new Vector3(4, 1, 4));
 			Gizmos.color = prevColour;
 		}
 		if(SouthTile != null)
 		{
 			Color prevColour = Gizmos.color;
 			Gizmos.color = Color.blue;
-			Gizmos.DrawWireCube(SouthTile.transform.position + new Vector3(0, 1, 0), new Vector3(4, 1, 4));
+			Gizmos.DrawWireCube(SouthTile.transform.position + new Vector3(0, 0.25f, 0), new Vector3(4, 1, 4));
 			Gizmos.color = prevColour;
 		}
 		if(EastTile != null)
 		{
 			Color prevColour = Gizmos.color;
 			Gizmos.color = new Color(1.0f, 0.8f, 0.8f, 255);
-			Gizmos.DrawWireCube(EastTile.transform.position + new Vector3(0, 1, 0), new Vector3(4, 1, 4));
+			Gizmos.DrawWireCube(EastTile.transform.position + new Vector3(0, 0.25f, 0), new Vector3(4, 1, 4));
 			Gizmos.color = prevColour;
 		}
 		if(WestTile != null)
 		{
 			Color prevColour = Gizmos.color;
 			Gizmos.color = Color.red;
-			Gizmos.DrawWireCube(WestTile.transform.position + new Vector3(0, 1, 0), new Vector3(4, 1, 4));
+			Gizmos.DrawWireCube(WestTile.transform.position + new Vector3(0, 0.25f, 0), new Vector3(4, 1, 4));
 			Gizmos.color = prevColour;
 		}
 	}
