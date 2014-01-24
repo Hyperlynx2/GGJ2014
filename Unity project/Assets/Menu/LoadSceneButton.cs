907290ca47f50ab21e36df37b3a9afb9544aeebc
Leonard Frankel <leonard@leonardfrankel.com>
2014-01-24 22:16:56 +1100

    Moved to a new folder, so stuff outside unity project can be added

---
+++ Unity project/Assets/Menu/LoadSceneButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadSceneButton : MonoBehaviour
+{
+	public float left;
+	public float top;
+	public float width;
+	public float height;
+	public string text;
+	public string sceneName;
+	public GUISkin skin;
+
+	void OnGUI()
+	{
+		GUI.skin = skin;
+		if(GUI.Button(new Rect(left, top, width, height), text))
+		{
+			Application.LoadLevel(sceneName);
+		}
+	}
+
+}
