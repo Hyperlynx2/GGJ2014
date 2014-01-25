d0eba209ceb314b22f3d1e1af6727fff5269345c
Leonard Frankel <leonard@leonardfrankel.com>
2014-01-25 11:25:18 +1100

    Two players, input switches between player 1 and player 2, turn timer. (issue #3)

--- Unity project/Assets/Level Assets/Player.cs
+++ Unity project/Assets/Level Assets/Player.cs
@@ -8,6 +8,16 @@ public class Player : MonoBehaviour
 	\********/
 	public Tile startTile;
 	public float movementSpeed;
+	public string player1HorizontalAxis;
+	public string player1VerticalAxis;
+	public string player2HorizontalAxis;
+	public string player2VerticalAxis;
+	public string[] playerNames;
+	
+	/// <summary>
+	/// How much time each player gets per turn.
+	/// </summary>
+	public float turnTime;
 	
 	/*other stuff*/	
 	private const float NORTH = 0f;
@@ -23,11 +33,25 @@ public class Player : MonoBehaviour
 	/// do not accept any movement change commands unless we're finished moving!
 	/// </summary>
 	private float _movementTimeRemaining;
-
-	// Use this for initi alization
+	
+	/// <summary>
+	/// Time remaining before contorl switches to the other player;
+	/// </summary>
+	private float _playerTurnRemaining;
+	
+	public enum PLAYER_NUM
+	{
+		P1,
+		P2
+	}
+	
+	private PLAYER_NUM _currentPlayer;
+	
+	// Use this for initialization
 	void Start ()
 	{
 		gameObject.transform.position = startTile.gameObject.transform.position;
+		_currentPlayer = PLAYER_NUM.P2;
 
 		_currentTile = startTile;
 		_heading = NORTH;
@@ -38,28 +62,70 @@ public class Player : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		/*no guaruntees that Start will get called only after the tiles have been generated, so
+		initialise here on the first frame*/
 		if(!_initialised)
 		{
 			_initialised = true;
 			_currentTile.SetConnectedTilesHighlighted(true);
-		}		
+		}
 		
+		UpdateTurnSwitch();
+		UpdateInput();	
+	}
+	
+	void OnGUI()
+	{
+		//TODO: make time remaining be formatted in minutes:seconds
+		GUI.Box (new Rect (500, 200,100,50), playerNames[(int)_currentPlayer] + "\n" + _playerTurnRemaining);
+	}
+
+	/// <summary>
+	/// Handle turn timer. Decrement time, switch controls if necessary.
+	/// </summary>
+	private void UpdateTurnSwitch()
+	{
+		_playerTurnRemaining -= Time.deltaTime;
+		
+		if(_playerTurnRemaining <= 0)
+		{
+		
+			if(_currentPlayer == PLAYER_NUM.P1)
+				_currentPlayer = PLAYER_NUM.P2;
+			else
+				_currentPlayer = PLAYER_NUM.P1;
+			
+			_playerTurnRemaining = turnTime;
+		}
+	}
+	
+	private void UpdateInput()
+	{
 		if(_movementTimeRemaining <= 0)
 		{
+			string horz = player1HorizontalAxis;
+			string vert = player1VerticalAxis;
+			
+			if(_currentPlayer == PLAYER_NUM.P2)
+			{
+				horz = player2HorizontalAxis;
+				vert = player2VerticalAxis;
+			}
+			
 			//TODO: take heading into consideration here		
-			if(Input.GetAxis ("Horizontal") < 0) //left
+			if(Input.GetAxis (horz) < 0) //left
 			{
 				MoveTo(_currentTile.WestTile);		
 			}
-			else if(Input.GetAxis("Horizontal") > 0) //right
+			else if(Input.GetAxis(horz) > 0) //right
 			{
 				MoveTo(_currentTile.EastTile);
 			}
-			else if(Input.GetAxis("Vertical") < 0) //down
+			else if(Input.GetAxis(vert) < 0) //down
 			{
 				MoveTo(_currentTile.SouthTile);
 			}
-			else if(Input.GetAxis("Vertical") > 0) //up
+			else if(Input.GetAxis(vert) > 0) //up
 			{
 				MoveTo(_currentTile.NorthTile);
 			}
@@ -73,9 +139,11 @@ public class Player : MonoBehaviour
 			if(_movementTimeRemaining < 0)
 				_movementTimeRemaining = 0;
 		}
-	
 	}
 	
+	/// <summary>
+	/// Handle movement to the destination tile.
+	/// </summary>
 	private void MoveTo(Tile destination)
 	{
 		if(destination != null)
