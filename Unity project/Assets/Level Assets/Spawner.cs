5a670097413f4ce914372407cc35986a8f7ed5df
Matt Dodd <doddzy39@gmail.com>
2014-01-25 23:34:38 +1100

    Modified GeneratelevelNavMesh script to work with the new level sizing

--- Unity project/Assets/Level Assets/Spawner.cs
+++ Unity project/Assets/Level Assets/Spawner.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class Spawner : MonoBehaviour {
 
 	public int NumSpawnedFlags = 0;
+	public int PointsPerFlag = 0;
 	
 	public List<GameObject> FlagInstances = new List<GameObject>();
 }
